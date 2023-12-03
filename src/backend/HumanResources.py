a70ea0922813935f0152c6f20b2c8dbc94f1e2ca
jcolano <juan_olano@yahoo.com>
2023-12-02 21:01:54 -0500

    JCO2

--- src/backend/HumanResources.py
+++ src/backend/HumanResources.py
@@ -5,7 +5,7 @@ import os
 CURRENT_DIR = os.getcwd()
 
 # Assuming your 'agents.json' file is in the same directory as the notebook
-FILE_PATH = os.path.join(CURRENT_DIR, "agents.json")
+FILE_PATH = os.path.join(CURRENT_DIR, 'agents.json')
 
 # Check if the file exists and read it, otherwise create an empty dictionary
 if os.path.exists(FILE_PATH):
@@ -14,37 +14,44 @@ if os.path.exists(FILE_PATH):
 else:
     sample_agent_json = {}
 
-
-class HumanResources:
+class HumanResources():
     def __init__(self, chat_initiator):
         self.agent_json = sample_agent_json
-        self.chat_initiator = chat_initiator
+        self.chat_initiator = chat_initiator 
 
     def __repr__(self):
-        return f"{self.agent_json}"
+        return f'{self.agent_json}'
 
     def _create(self, new_agent):
-        if new_agent["role"] in self.agent_json:
-            raise ValueError(f"Agent with role '{new_agent['role']}' already exists.")
+        # Check if 'role' key exists in new_agent dictionary
+        role = new_agent.get('role')
+        if role is None:
+            return "Missing 'role' key, agent not created"
 
-        new_agent["agent_type"] = (
-            "userproxy" if new_agent["role"] == self.chat_initiator else "assistant"
-        )
-        self.agent_json[new_agent["role"]] = new_agent
+        new_agent['agent_type'] = 'userproxy' if role == self.chat_initiator else 'assistant'
+        new_agent['description'] = f"{role} expert"
+        new_agent['human_input_mode'] = ""
+        new_agent['skills'] = [role]
+
+        if role not in self.agent_json or not isinstance(self.agent_json[role], list):
+            self.agent_json[role] = []
+        self.agent_json[role].append(new_agent)
         self._save_to_file()
 
-        return self.agent_json[new_agent["role"]]
+        return new_agent
 
     def _read(self, agent_role):
         return self.agent_json.get(agent_role, "Agent role not found")
 
     def _update(self, agent_role, new_agent):
         if agent_role not in self.agent_json:
-            raise ValueError(f"Agent with role '{agent_role}' does not exist.")
+            return "Agent role not found"
 
-        new_agent["agent_type"] = (
-            "userproxy" if new_agent["role"] == self.chat_initiator else "assistant"
-        )
+        new_agent['agent_type'] = 'userproxy' if new_agent['role'] == self.chat_initiator else 'assistant'
+        new_agent['description'] = new_agent.get('description', f"{new_agent['role']} expert")
+        new_agent['human_input_mode'] = new_agent.get('human_input_mode', "")
+        new_agent['skills'] = new_agent.get('skills', [new_agent['role']])
+
         self.agent_json[agent_role] = new_agent
         self._save_to_file()
         return self.agent_json[agent_role]
@@ -59,29 +66,22 @@ class HumanResources:
     def agent_object(self, agent_role):
         return self.agent_json.get(agent_role, "Agent role not found")
 
+
     def select_agents(self):
         return list(self.agent_json.keys())
-
+    
     def get_complete(self, roles_list):
         result = []
         for role in roles_list:
-            for agent_name, agent_details in self.agent_json.items():
-                if agent_details["role"] == role:
-                    result.append(agent_details)
+            agents = self.agent_json.get(role, [])
+            if agents:
+                result.extend(agents)  # Ensure that agents is a list of dictionaries
         return result
 
     def _save_to_file(self):
-        with open(FILE_PATH, "w") as f:
+        with open(FILE_PATH, 'w') as f:
             json.dump(self.agent_json, f, indent=4)
 
-
-# # Example usage
-# hr = HumanResources()
-# new_agent = {"name": "Agent Smith", "role": "Analyst", "description": "Data Analysis"}
-# hr._create(new_agent)
-
-# # Print updated agents
-# print(hr.select_agents())
 
 
 if __name__ == "__main__":
