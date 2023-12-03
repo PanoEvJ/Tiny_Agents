17253cb1a66b7bf7d95c69dd80ece632a325e32f
Pano <panosevag@gmail.com>
2023-12-03 01:32:59 +0100

    fixes

--- src/backend/HumanResources.py
+++ src/backend/HumanResources.py
@@ -3,17 +3,18 @@ import os
 
 # load json file from this file's directory
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-FILE_PATH = os.path.join(CURRENT_DIR, 'agents.json')
+FILE_PATH = os.path.join(CURRENT_DIR, "agents.json")
 
 with open(FILE_PATH) as f:
     sample_agent_json = json.load(f)
 
-class HumanResources():
+
+class HumanResources:
     def __init__(self):
         self.agent_json = sample_agent_json
 
     def __repr__(self):
-        return f'{self.agent_json}'
+        return f"{self.agent_json}"
 
     def _create(self, new_agent):
         self.agent_json[new_agent["role"]] = new_agent
@@ -40,31 +41,36 @@ class HumanResources():
         return list(self.agent_json.keys())
 
     def _save_to_file(self):
-        with open(FILE_PATH, 'w') as f:
+        with open(FILE_PATH, "w") as f:
             json.dump(self.agent_json, f, indent=4)
 
-# Example usage
-hr = HumanResources()
-new_agent = {"name": "Agent Smith", "role": "Analyst", "description": "Data Analysis"}
-hr._create(new_agent)
 
-# Print updated agents
-print(hr.select_agents())
+# # Example usage
+# hr = HumanResources()
+# new_agent = {"name": "Agent Smith", "role": "Analyst", "description": "Data Analysis"}
+# hr._create(new_agent)
 
-    
+# # Print updated agents
+# print(hr.select_agents())
+
 
 if __name__ == "__main__":
     hr = HumanResources()
     # assert _create() returns a new agent object: {'name': 'finance', 'title': 'finance manager'}
-    assert hr._create({"name": "finance", "title": "finance manager"}) == {'name': 'finance', 'title': 'finance manager'}
+    assert hr._create({"name": "finance", "title": "finance manager"}) == {
+        "name": "finance",
+        "title": "finance manager",
+    }
     # assert _read() returns an agent object: {'name': 'finance', 'title': 'finance manager'}
-    assert hr._read("finance") == {'name': 'finance', 'title': 'finance manager'}
+    assert hr._read("finance") == {"name": "finance", "title": "finance manager"}
     # assert _update() returns an updated agent object: {'name': 'finance', 'title': 'senior finance manager'}
-    assert hr._update("finance", {"name": "finance", "title": "senior finance manager"}) == {'name': 'finance', 'title': 'senior finance manager'}
+    assert hr._update(
+        "finance", {"name": "finance", "title": "senior finance manager"}
+    ) == {"name": "finance", "title": "senior finance manager"}
     # assert _delete() returns json all agents without the deleted agent, just check the names of the agents: ['sales', 'marketing', 'engineer']
     temp_json = hr._delete("finance")
-    assert list(temp_json.keys()) == ['sales', 'marketing', 'engineer']
+    assert list(temp_json.keys()) == ["sales", "marketing", "engineer"]
     # assert select_agents() returns a list of all agents: ['sales', 'marketing', 'engineer']
-    assert hr.select_agents() == ['sales', 'marketing', 'engineer']
+    assert hr.select_agents() == ["sales", "marketing", "engineer"]
 
     print("All tests passed!")
--- src/backend/app.py
+++ src/backend/app.py
@@ -9,6 +9,17 @@ import openai
 from typing import Dict, List, Optional, Union
 import os
 from dotenv import load_dotenv
+from HumanResources import HumanResources
+from AgentSelector import AgentSelector
 
+# load_dotenv()
 
-load_dotenv()
+hr = HumanResources()
+all_available_agents = hr.select_agents()
+print(all_available_agents)
+
+task = "revenue in the east coast is falling and the competitor is doing great with their product."
+selector = AgentSelector(task=task, available_agents=all_available_agents, n_agents=3)
+selected_agents = selector.run_selection()
+print("*" * 10, selected_agents)
+selected_agents = []
